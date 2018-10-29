29bdb717f7053c4eea5d852a01d7e7f53710a77c
ediblesushi <chrisdoucette15@gmail.com>
2018-10-29 16:32:02 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ediblepaste',
-    version='1.1',
+    version='1.2',
     author='Chris Doucette',
     author_email='chrisdoucette15@gmail.com',
     description='Wrapper for paste sites API',
